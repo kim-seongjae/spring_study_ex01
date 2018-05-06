a99ea9d26475c9c5c8c8b6313c94ba83eec3689b
asdf7209 <asdf7209@naver.com>
2018-05-06 20:55:36 +0900

    UriComponentsBuilder로 페이징처리 개선(HTML링크)

--- src/main/java/org/zerock/domain/PageMaker.java
+++ src/main/java/org/zerock/domain/PageMaker.java
@@ -1,6 +1,7 @@
 package org.zerock.domain;
 
-
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public class PageMaker {
 	
@@ -14,33 +15,43 @@ public class PageMaker {
 	
 	private Criteria cri;
 	
+	
+	
 	public void setCri(Criteria cri) {
 		this.cri = cri;
 	}
 	
+	
+	
 	public void setTotalCount(int totalCount) {
 		this.totalCount=totalCount;
-		
 		calcData();
 	}
 	
+	
 	private void calcData() {
 		endPage = (int)(Math.ceil(cri.getPage() / (double)displayPageNum)*displayPageNum);
-		
 		startPage = (endPage - displayPageNum) + 1;
-		
 		int tempEndPage = (int)(Math.ceil(totalCount)/(double)cri.getPerPageNum());
-		
 		if(endPage > tempEndPage) {
 			endPage = tempEndPage;
 		}
-		
 		prev = startPage == 1 ? false : true;
-		
 		next = endPage * cri.getPerPageNum() >= totalCount ? false : true;
-		
 	}
+	
+	
 
+	public String makeQuery(int page) {
+		UriComponents uriComponents = 
+				UriComponentsBuilder.newInstance()
+				.queryParam("page", page )
+				.queryParam("perPageNum", cri.getPerPageNum())
+				.build();
+		return uriComponents.toUriString();
+	}
+	
+	
 	@Override
 	public String toString() {
 		return "PageMaker [totalCount=" + totalCount + ", startPage=" + startPage + ", endPage=" + endPage + ", prev="
