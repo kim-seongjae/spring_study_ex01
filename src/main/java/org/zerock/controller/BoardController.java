b4c85782133523758eae7ad54bde3bc80799005f
asdf7209 <asdf7209@naver.com>
2018-05-07 02:57:15 +0900

    목록페이지와 정보 유지하기

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -58,6 +59,11 @@ public class BoardController {
 		model.addAttribute(service.read(bno));
 	}
 	
+	@RequestMapping(value="/readPage", method=RequestMethod.GET)
+	public void readPage(@RequestParam("bno") int bno, @ModelAttribute("cri") Criteria cri, Model model) throws Exception{
+		model.addAttribute(service.read(bno));
+	}
+	
 	
 	@RequestMapping(value="/remove", method=RequestMethod.POST)
 	public String remove(@RequestParam("bno") int bno, RedirectAttributes rttr)throws Exception{
@@ -101,9 +107,18 @@ public class BoardController {
 		pageMaker.setTotalCount(service.listCountCriteria(cri));
 		
 		model.addAttribute("pageMaker", pageMaker);
-		
-		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
