0355170872abd854c657cf1ee8a7a3b2b9220664
asdf7209 <asdf7209@naver.com>
2018-06-23 16:58:36 +0900

    Reply달았을때 Board쪽에도 Cnt변경되도록 수정

--- src/main/java/org/zerock/service/ReplyServiceImpl.java
+++ src/main/java/org/zerock/service/ReplyServiceImpl.java
@@ -5,44 +5,51 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
+import org.zerock.persistence.BoardDAO;
 import org.zerock.persistence.ReplyDAO;
 
 @Service
 public class ReplyServiceImpl implements ReplyService{
 	
 	@Inject
-	private ReplyDAO dao;
+	private ReplyDAO replayDAO;
+	private BoardDAO boardDAO;
 
+	@Transactional
 	@Override
 	public void addReply(ReplyVO vo) throws Exception {
-		dao.create(vo);
+		replayDAO.create(vo);
+		boardDAO.updateReplyCnt(vo.getBno(),1);
 	}
 
 	@Override
 	public List<ReplyVO> listReply(Integer bno) throws Exception {
-		return dao.list(bno);
+		return replayDAO.list(bno);
 	}
 
 	@Override
 	public void modifyReply(ReplyVO vo) throws Exception {
-		dao.update(vo);
+		replayDAO.update(vo);
 	}
-
+	@Transactional
 	@Override
 	public void removeReply(Integer rno) throws Exception {
-		dao.delete(rno);
+		int bno=replayDAO.getBno(rno);
+		replayDAO.delete(rno);
+		boardDAO.updateReplyCnt(bno, -1);
 	}
 
 	@Override
 	public List<ReplyVO> listReplyPage(Integer bno,Criteria cri) throws Exception {
-		return dao.listPage(bno, cri);
+		return replayDAO.listPage(bno, cri);
 	}
 
 	@Override
 	public int count(Integer bno) throws Exception {
-		return dao.count(bno);
+		return replayDAO.count(bno);
 	}
 
 }
