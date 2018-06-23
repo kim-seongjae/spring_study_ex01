01b30f8117bc68f79d23dea6e07e7a01e17c161c
asdf7209 <asdf7209@naver.com>
2018-06-23 16:46:03 +0900

    게시글의 댓글 숫자 조회(VO및 Mapper설정)

--- src/main/java/org/zerock/domain/BoardVO.java
+++ src/main/java/org/zerock/domain/BoardVO.java
@@ -10,8 +10,15 @@ public class BoardVO {
 	private String writer;
 	private Date regdate;
 	private int viewcnt;
+	private int replycnt;
 	public Integer getBno() {
 		return bno;
+	}
+	public int getReplycnt() {
+		return replycnt;
+	}
+	public void setReplycnt(int replycnt) {
+		this.replycnt = replycnt;
 	}
 	public void setBno(Integer bno) {
 		this.bno = bno;
@@ -43,13 +50,13 @@ public class BoardVO {
 	public int getViewcnt() {
 		return viewcnt;
 	}
-	public void setViewcnt(int viewcnt) {
-		this.viewcnt = viewcnt;
-	}
 	@Override
 	public String toString() {
 		return "BoardVO [bno=" + bno + ", title=" + title + ", content=" + content + ", writer=" + writer + ", regdate="
-				+ regdate + ", viewcnt=" + viewcnt + "]";
+				+ regdate + ", viewcnt=" + viewcnt + ", replycnt=" + replycnt + "]";
+	}
+	public void setViewcnt(int viewcnt) {
+		this.viewcnt = viewcnt;
 	}
 	
 }
