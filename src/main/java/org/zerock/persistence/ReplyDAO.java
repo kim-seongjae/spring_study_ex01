d76e2aadb1a60adb13ac9b60b54cb970f5693f67
asdf7209 <asdf7209@naver.com>
2018-06-23 16:56:05 +0900

    Transaction 사용 전 준비

--- src/main/java/org/zerock/persistence/ReplyDAO.java
+++ src/main/java/org/zerock/persistence/ReplyDAO.java
@@ -7,14 +7,16 @@ import org.zerock.domain.ReplyVO;
 
 public interface ReplyDAO {
 	public List<ReplyVO> list(Integer bno) throws Exception;
-	
+
 	public void create(ReplyVO vo) throws Exception;
-	
+
 	public void update(ReplyVO vo) throws Exception;
-	
+
 	public void delete(Integer bno) throws Exception;
-	
+
 	public List<ReplyVO> listPage(Integer bno, Criteria cri) throws Exception;
-	
+
 	public int count(Integer bno) throws Exception;
+
+	public int getBno(Integer bno) throws Exception;
 }
